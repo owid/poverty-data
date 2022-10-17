b5fe3c4cab17b1e75ca9181dbf781a3611f73f23
Pablo Rosado <pabloarosado@gmail.com>
2022-10-17 10:54:51 +0200

    refactor: Remove old shared module

--- scripts/shared.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-# Define path to output directory.
-OUTPUT_DIR = Path(__file__).parent.parent
-# Define base names of output files.
-OUTPUT_FILE_BASE_NAME = "owid-poverty-data"
-# Define codebook path.
-CODEBOOK_PATH = OUTPUT_DIR / "owid-poverty-codebook.csv"
